68718ff079afe2d84587c51a31446f89fab5cfd7
Aayush <sapkotaaayush2062@gmal.com>
2024-08-24 18:52:05 +0545

    added basic animation with timeline

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ var menu = document.querySelector("#nav i")
 var cross = document.querySelector("#full i")
 tl.to("#full", {
     right: 0,
-    duration:1,
+    duration:0.5,
 })
 
 tl.from("#full h4", {
     x: 150,
-    duration: 0.7,
+    duration: 0.5,
     stagger: 0.2,
     opacity:0,
 })
@@ -17,3 +17,12 @@ tl.from("#full i",{
     opacity:0,
 })
 
+tl.pause()
+
+menu.addEventListener("click" , function(){
+    tl.play()
+})
+
+cross.addEventListener("click",function(){
+    tl.reverse()
+})
