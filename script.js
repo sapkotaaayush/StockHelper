2b408d4803d4f66cb52912a4ed0b23b2381465ee
Aayush <sapkotaaayush2062@gmal.com>
2024-08-24 21:45:10 +0545

    Added basic animation

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var tl = gsap.timeline()
 var menu = document.querySelector("#nav i")
 var cross = document.querySelector("#full i")
+
+function splitTheText(){
+    var h1 = document.querySelector("#title h1")
+    h1Text = h1.textContent
+
+    var clutter = ""
+var splittedText = h1Text.split("")
+console.log(splittedText)
+    splittedText.forEach(function(letters){
+        console.log(letters)
+        clutter += `<span>${letters}</span>`
+    })
+    console.log(clutter)
+    h1.innerHTML =  clutter
+}
+
+splitTheText()
+
 tl.to("#full", {
     right: 0,
     duration:0.5,
@@ -25,4 +43,13 @@ menu.addEventListener("click" , function(){
 
 cross.addEventListener("click",function(){
     tl.reverse()
+})
+
+
+gsap.from("h1 span", {
+    y: 100,
+    opacity: 0,
+    duration:1,
+    delay: 0.5,
+    stagger:0.3,
 })
